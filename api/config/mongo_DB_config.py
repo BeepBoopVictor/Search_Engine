f1c5d189d98399202afeb9d636a4c0b6b9bf65d9
Victor <145380029+BeepBoopVictor@users.noreply.github.com>
2024-10-19 12:33:56 +0100

    last commit

--- api/config/mongo_DB_config.py
+++
@@ -1,6 +0,0 @@
-import os
-from pymongo import MongoClient
-
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-mongo_client = MongoClient(MONGO_URI)
-mongo_db = mongo_client["book_search_engine"]
--- indexer/metadata/StoreTXTMetadata.py
+++
@@ -1,5 +0,0 @@
-
-def metadata_index_txt_store(metadata: dict, file: str):
-    with open(file, 'a') as f:
-        for key, value in metadata.items():
-            f.write(f"{{'{key}': {value}}}\n")
